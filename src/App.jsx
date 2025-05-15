632c7bb6327dc1e460da5362f7bf417fb1e66a17
AxelStrelow <68808094+AxelStrelow@users.noreply.github.com>
2025-05-15 21:57:44 +0100

    Changed to add a folder

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1 style={{ color: 'blue' }}>Speeduko</h1>
+      <p>Sudoku game coming soon...</p>
+    </div>
+  );
+}
