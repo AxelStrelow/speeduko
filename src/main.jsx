de4a4786431f918323c296662670fcd22da1536f
AxelStrelow <68808094+AxelStrelow@users.noreply.github.com>
2025-05-15 23:21:41 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // main.jsx
-import MainApp from './MainApp';
+import MainApp from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
