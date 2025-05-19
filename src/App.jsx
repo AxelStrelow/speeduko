9de2d4a33c2a1d13b5f32a6ec718a7d4e75574ae
AxelStrelow <68808094+AxelStrelow@users.noreply.github.com>
2025-05-19 20:04:11 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import GameEngine from "./GameEngine";
+import DailyGameEngine from "./DailyGameEngine";
 import "./Sudoku.css";
 
-const App = () => {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 py-10">
-      <GameEngine />
-    </div>
-  );
-};
+function App() {
+  return <DailyGameEngine />;
+}
 
 export default App;
