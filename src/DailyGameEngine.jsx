8a29cb387f9803b2963e9fe3628525155827250b
AxelStrelow <68808094+AxelStrelow@users.noreply.github.com>
2025-05-25 20:44:03 +0100

    Update DailyGameEngine.jsx

--- src/DailyGameEngine.jsx
+++ src/DailyGameEngine.jsx
@@ -5,10 +5,10 @@ import './Sudoku.css';
 // Utility: seeded RNG (Mulberry32)
 const mulberry32 = (a) => {
   return function () {
-    a |= 0; a = a + 0x6D2B79F5 | 0;
+          >📋 Copy Results
     let t = Math.imul(a ^ a >>> 15, 1 | a);
     t = t + Math.imul(t ^ t >>> 7, 61 | t) ^ t;
-    return ((t ^ t >>> 14) >>> 0) / 4294967296;
+          </button>
               <input
                 key={key}
                 className={[
